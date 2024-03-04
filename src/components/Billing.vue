8b8aebf16a7a2fd3b703e6404937990204e6be40
lananhvu2001 <vulananh2001@gmail.com>
2024-03-04 23:26:50 +0700

    add raw billing

--- src/components/Billing.vue
+++ src/components/Billing.vue
@@ -1,11 +1,31 @@
 <script setup>
+import {ref, computed} from 'vue'
+const custom = ref('Custom')
+const billValue = ['$10','$20','$50',custom.value]
 
+const selectedValue = ref('')
+
+const currentBalance = computed(() => {
+    return selectedValue.value;
+});
 </script>
 
 <template>
-  Billing
+    <section style="border: 1px solid; padding:30px; width: 350px">
+        <div style="margin-top:10px; margin-bottom:20px">
+            <p style="padding-bottom: 15px">Your balance</p>
+            <p style="padding-bottom: 30px">Current balance: {{(currentBalance!=='Custom')?currentBalance:''}}</p>
+            <p>Buy credit</p>
+        </div>
+        <div style="display: flex">
+                <div v-for="(item,index) in billValue" :key="index" style="margin: 5px">
+                    <button class="custom-button" @click="selectedValue=item">{{item}}</button>
+                </div>
+        </div>
+        <div>
+            <button class="mt-2" style="background-color: lightgreen; width: 280px">Buy credit</button>
+        </div>
+    </section>
+
+
 </template>
-
-<style scoped>
-
-</style>
--- src/components/Setting.vue
+++ src/components/Setting.vue
@@ -1,11 +1,37 @@
 <script setup>
+import {ref} from 'vue'
+import {userAPI} from '@/api';
+import {useRouter} from 'vue-router';
+//import '../../style/style.css'
 
+const currentPwd = ref()
+const newPwd = ref()
+const confirmPwd = ref()
+const check = ref(true)
+
+const changePass = async()=> {
+    if (confirmPwd.value!=='') {
+        check.value = newPwd.value === confirmPwd.value;
+        if (check.value===true) {
+            await userAPI.changePassword({password:newPwd.value})
+        } else {
+            console.log("Password doesn't match!")
+        }
+    } else {
+        check.value = true
+    }
+}
 </script>
 
 <template>
-  Setting
+    <div>
+        <h1>Change password</h1>
+        <p><t-password v-model="currentPwd" class="w-50" label="Current password"/></p>
+        <p><t-password v-model="newPwd" class="w-50" label="New password"/></p>
+        <p><t-password v-model="confirmPwd" class="w-50" label="Confirm password"/></p>
+        <p v-show = "!check" class="red-text">Password don't match. Please try again</p>
+    </div>
+    <div>
+        <button @click="changePass">Change password</button>
+    </div>
 </template>
-
-<style scoped>
-
-</style>
