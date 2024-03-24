3b8dcb9f82642c38b49ec12b5e064003ccdf279b
lananhvu2001 <147015163+lananhvu2001@users.noreply.github.com>
2024-03-24 17:17:28 +0700

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000';
 export const WS_URL = import.meta.env.VITE_WS_URL || 'http://localhost:4000';
-export const MONGODB_HOSTS = import.meta.env.VITE_MONGODB_HOSTS || "85.239.233.195:37017,194.233.82.10:37017,144.91.123.235:37018";
+export const MONGODB_HOSTS = import.meta.env.VITE_MONGODB_HOSTS || "85.239.233.195:37017,194.233.82.10:37017,144.91.123.235:37017";
 export const MONGODB_REPL_NAME = import.meta.env.VITE_MONGODB_REPL_NAME || "s3";
